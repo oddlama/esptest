72c830ed80069ec04801616b8ed36ba864936e1e
oddlama <oddlama@oddlama.org>
2024-09-06 18:05:38 +0200

    feat: blinky but spawner busy

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,45 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use defmt::info;
-use defmt_rtt as _;
+use esp_println as _;
+use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
-    peripherals::Peripherals,
-    prelude::*,
-    rtc_cntl::Rtc,
-    system::SystemControl,
-    timer::{timg::TimerGroup, OneShotTimer},
+    gpio::{ErasedPin, Io, Level, Output, Pin},
+    timer::timg::TimerGroup,
 };
-use static_cell::make_static;
 
-#[main]
+#[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
 
-    // Enable the RWDT watchdog timer:
-    let mut rtc = Rtc::new(peripherals.LPWR, None);
-    rtc.rwdt.set_timeout(2.secs());
-    rtc.rwdt.enable();
-    info!("RWDT watchdog enabled!");
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    info!("defmt init");
 
-    // Initialize the SYSTIMER peripheral, and then Embassy:
-    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
-    let timers = [OneShotTimer::new(timg0.timer0.into())];
-    let timers = make_static!(timers);
-    esp_hal_embassy::init(&clocks, timers);
-    info!("Embassy initialized!");
+    let led1 = Output::new(io.pins.gpio13.degrade(), Level::High);
+    let led2 = Output::new(io.pins.gpio12.degrade(), Level::High);
+    let led3 = Output::new(io.pins.gpio11.degrade(), Level::High);
+    spawner.spawn(blink(led1)).unwrap();
+    spawner.spawn(blink(led2)).unwrap();
+    spawner.spawn(blink(led3)).unwrap();
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
-
-    // Periodically feed the RWDT watchdog timer when our tasks are not running:
     loop {
-        rtc.rwdt.feed();
+        info!("nop");
         Timer::after(Duration::from_secs(1)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn blink(mut pin: Output<'static, ErasedPin>) {
+    loop {
+        info!("blink");
+        pin.set_low();
+        Timer::after(Duration::from_millis(50)).await;
+        pin.set_high();
+        Timer::after(Duration::from_millis(950)).await;
     }
 }
