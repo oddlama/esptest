1483cd6424d5b247a8af327a11cfa7e445ad58f4
oddlama <oddlama@oddlama.org>
2024-10-02 22:01:16 +0200

    feat: show hello world on ssd1306

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,32 @@
 use core::cell::RefCell;
 
 use defmt::{info, unwrap};
-use display_interface_spi::SPIInterface;
-use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
+use embassy_embedded_hal::shared_bus::blocking::i2c::I2cDevice;
 use embassy_executor::Spawner;
-use embassy_sync::blocking_mutex::{raw::NoopRawMutex, Mutex};
+use embassy_sync::blocking_mutex::{raw::CriticalSectionRawMutex, Mutex};
 use embassy_time::{Duration, Timer};
-use embedded_graphics::draw_target::DrawTarget;
-use embedded_graphics::{pixelcolor::Rgb666, prelude::RgbColor};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
 use esp_backtrace as _;
 use esp_hal::{
-    delay::Delay,
     gpio::{Io, Level, Output, Pin},
     i2c::I2C,
+    peripherals::I2C0,
     prelude::_fugit_RateExtU32,
-    spi::{master::Spi, FullDuplexMode, SpiMode},
     timer::timg::TimerGroup,
+    Blocking,
 };
 use esp_println as _;
-use mipidsi::models::ST7789;
+use ssd1306::mode::DisplayConfig;
+use ssd1306::{prelude::DisplayRotation, I2CDisplayInterface, Ssd1306};
 use static_cell::StaticCell;
 
-static SPI_BUS: StaticCell<
-    Mutex<
-        NoopRawMutex,
-        RefCell<esp_hal::spi::master::Spi<'_, esp_hal::peripherals::SPI2, FullDuplexMode>>,
-    >,
-> = StaticCell::new();
+static I2C_BUS: StaticCell<Mutex<CriticalSectionRawMutex, RefCell<I2C<'static, I2C0, Blocking>>>> =
+    StaticCell::new();
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -38,51 +38,55 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let delay = Delay::new();
+    // let delay = Delay::new();
 
     // Select internal antenna
     let _ = Output::new(io.pins.gpio14.degrade(), Level::Low);
 
     // I2C bus
-    let sda = io.pins.gpio21;
-    let scl = io.pins.gpio22;
-    let _i2c = I2C::new(peripherals.I2C0, sda, scl, 100u32.kHz());
-
-    // SPI bus
-    let sck = io.pins.gpio19;
-    let mosi = io.pins.gpio18;
-    let spi = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0)
-        .with_mosi(mosi)
-        .with_sck(sck);
-    let spi = Mutex::<NoopRawMutex, _>::new(RefCell::new(spi));
-    let spi = SPI_BUS.init(spi);
-
-    let cs = Output::new(io.pins.gpio5.degrade(), Level::Low); // dummy
-    let display_device = SpiDevice::new(spi, cs);
-    let dc = Output::new(io.pins.gpio2.degrade(), Level::Low);
-    unwrap!(spawner.spawn(ui(delay, display_device, dc)));
+    let sda = io.pins.gpio22;
+    let scl = io.pins.gpio23;
+    let i2c_bus = I2C::new(peripherals.I2C0, sda, scl, 200u32.kHz());
+    let i2c_bus = I2C_BUS.init(Mutex::new(RefCell::new(i2c_bus)));
 
     let led1 = Output::new(io.pins.gpio15.degrade(), Level::High);
     unwrap!(spawner.spawn(blink(led1)));
+    unwrap!(spawner.spawn(display(I2cDevice::new(i2c_bus))));
 }
 
 #[embassy_executor::task]
-async fn ui(
-    mut delay: Delay,
-    display_device: SpiDevice<
-        'static,
-        NoopRawMutex,
-        esp_hal::spi::master::Spi<'_, esp_hal::peripherals::SPI2, FullDuplexMode>,
-        Output<'static>,
-    >,
-    dc: Output<'static>,
+async fn display(
+    i2c_device: I2cDevice<'static, CriticalSectionRawMutex, I2C<'static, I2C0, Blocking>>,
 ) {
-    let di = SPIInterface::new(display_device, dc);
+    info!("Initialized display!");
+    let interface = I2CDisplayInterface::new(i2c_device);
+    let mut display = Ssd1306::new(
+        interface,
+        ssd1306::size::DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    )
+    .into_buffered_graphics_mode();
+    display.init().unwrap();
 
-    let mut display = mipidsi::Builder::new(ST7789, di)
-        .init(&mut delay)
-        .expect("No 1");
-    display.clear(Rgb666::WHITE.into()).expect("No 2");
+    loop {
+        display.clear(BinaryColor::Off).unwrap();
+        let text_style = MonoTextStyleBuilder::new()
+            .font(&FONT_6X10)
+            .text_color(BinaryColor::On)
+            .build();
+
+        Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        info!("update display!");
+        display.flush().unwrap();
+        Timer::after_millis(1000).await;
+    }
 }
 
 #[embassy_executor::task]
